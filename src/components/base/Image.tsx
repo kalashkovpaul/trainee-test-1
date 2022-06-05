5fc2f17835cab853ae5ed707a8cdc63ffbcd650d
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-06-05 15:27:00 +0300

    Task 1 is done

--- src/components/base/Image.tsx
+++ src/components/base/Image.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react';
 
-export default (props) => {
+type ImageProps = {
+	src?: string,
+	className?: string,
+	height?: string | number,
+	width?: string | number,
+}
+
+// Задание: обновить компонент Image таким образом, чтобы он отрисовывал изображение 
+// с помощью тега div (с class="..."), а не img;
+// Решение: заменим на тег div с canvas внутри, в него через url передадим src изображения
+export const Image: React.FunctionComponent<ImageProps> = ({src = '', className, ...props}: ImageProps) => {
+	const imageURLStyle = React.useMemo(
+        () => ({
+            ['backgroundImage']: `url(${src})`
+        }),
+        [src]
+    );
+	const additionalStyles = React.useMemo(
+		() => Object.keys(props).reduce((result: any, currentStyle) => (
+			{
+				...result,
+				[currentStyle]: Number(props[currentStyle]) || props[currentStyle]
+			}
+		), {}),
+		[src, props]
+	);
+	console.log(additionalStyles);
+
 	return (
-		<img
-			alt=""
-			{...props}
-		/>
+		<div className={className}>
+			<canvas style={{...imageURLStyle, ...additionalStyles}}/>
+		</div>
 	);
 };
+
+export default Image;
